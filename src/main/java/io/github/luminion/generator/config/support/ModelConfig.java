7299b624168334f10dced176a1337f4fa25f1ac6
lo <604647004@qq.com>
2025-11-11 13:48:00 +0800

    feat(config): 新增ModelAdapter并优化ModelConfig配置类
    - 新增ModelAdapter适配器类，提供链式调用配置功能
    -为ModelConfig添加@Data注解，简化getter/setter代码- 移除原有的内部Adapter类及相关方法
    - 优化导入包处理逻辑，支持多类型DTO导入包管理
    - 添加查询DTO和VO继承实体类的配置选项
    - 支持编辑排除字段和列的灵活配置

--- src/main/java/io/github/luminion/generator/config/support/ModelConfig.java
+++ src/main/java/io/github/luminion/generator/config/support/ModelConfig.java
@@ -6,6 +6,7 @@ import io.github.luminion.generator.config.po.TableField;
 import io.github.luminion.generator.config.po.TableField.MetaInfo;
 import io.github.luminion.generator.config.po.TableInfo;
 import io.github.luminion.generator.fill.ITemplate;
+import lombok.Data;
 import lombok.Getter;
 
 import java.util.*;
@@ -16,31 +17,33 @@ import java.util.stream.Collectors;
  *
  * @author luminion
  */
+@Data
 public class ModelConfig implements ITemplate {
 
     /**
      * 查询dto继承实体类
      */
-    @Getter
     protected boolean queryDTOExtendsEntity;
 
     /**
      * vo继承实体类
      */
-    @Getter
     protected boolean queryVOExtendsEntity;
 
     /**
      * 编辑排除字段
      */
-    @Getter
     protected Set<String> editExcludeFields = new HashSet<>();
 
     /**
      * 编辑排除数据库列
      */
-    @Getter
     protected Set<String> editExcludeColumns = new HashSet<>();
+    
+    protected Set<String> insertDTOImportPackages = new HashSet<>();
+    protected Set<String> updateDTOImportPackages = new HashSet<>();
+    protected Set<String> queryDTOImportPackages = new HashSet<>();
+    protected Set<String> queryVOImportPackages = new HashSet<>();
 
     @Override
     public Map<String, Object> renderData(TableInfo tableInfo) {
@@ -50,25 +53,25 @@ public class ModelConfig implements ITemplate {
         data.put("editExcludeFields", this.editExcludeFields);
         data.put("editExcludeColumns", this.editExcludeColumns);
 
-        Set<String> insertDTOImportPackages = this.insertDTOImportPackages(tableInfo);
+        insertDTOImportPackages.addAll(this.insertDTOImportPackages(tableInfo));
         List<String> insertDTOImportPackages4Framework = insertDTOImportPackages.stream().filter(pkg -> !pkg.startsWith("java")).collect(Collectors.toList());
         data.put("insertDTOImportPackages4Framework", insertDTOImportPackages4Framework);
         List<String> insertDTOImportPackages4Java = insertDTOImportPackages.stream().filter(pkg -> pkg.startsWith("java")).collect(Collectors.toList());
         data.put("insertDTOImportPackages4Java", insertDTOImportPackages4Java);
 
-        Set<String> updateDTOImportPackages = this.updateDTOImportPackages(tableInfo);
+        updateDTOImportPackages.addAll(this.updateDTOImportPackages(tableInfo));
         List<String> updateDTOImportPackages4Framework = updateDTOImportPackages.stream().filter(pkg -> !pkg.startsWith("java")).collect(Collectors.toList());
         data.put("updateDTOImportPackages4Framework", updateDTOImportPackages4Framework);
         List<String> updateDTOImportPackages4Java = updateDTOImportPackages.stream().filter(pkg -> pkg.startsWith("java")).collect(Collectors.toList());
         data.put("updateDTOImportPackages4Java", updateDTOImportPackages4Java);
 
-        Set<String> queryDTOImportPackages = this.queryDTOImportPackages(tableInfo);
+        queryDTOImportPackages.addAll(this.queryDTOImportPackages(tableInfo));
         List<String> queryDTOImportPackages4Framework = queryDTOImportPackages.stream().filter(pkg -> !pkg.startsWith("java")).collect(Collectors.toList());
         data.put("queryDTOImportPackages4Framework", queryDTOImportPackages4Framework);
         List<String> queryDTOImportPackages4Java = queryDTOImportPackages.stream().filter(pkg -> pkg.startsWith("java")).collect(Collectors.toList());
         data.put("queryDTOImportPackages4Java", queryDTOImportPackages4Java);
 
-        Set<String> queryVOImportPackages = this.queryVOImportPackages(tableInfo);
+        queryVOImportPackages.addAll(this.queryVOImportPackages(tableInfo));
         List<String> queryVOImportPackages4Framework = queryVOImportPackages.stream().filter(pkg -> !pkg.startsWith("java")).collect(Collectors.toList());
         data.put("queryVOImportPackages4Framework", queryVOImportPackages4Framework);
         List<String> queryVOImportPackages4Java = queryVOImportPackages.stream().filter(pkg -> pkg.startsWith("java")).collect(Collectors.toList());
@@ -223,58 +226,6 @@ public class ModelConfig implements ITemplate {
 
         return importPackages;
     }
-
-    public Adapter adapter() {
-        return new Adapter(this);
-    }
-
-    public static class Adapter {
-        private final ModelConfig config;
-
-        public Adapter(ModelConfig config) {
-            this.config = config;
-        }
-
-        /**
-         * 查询dto继承实体类
-         *
-         * @return this
-         */
-        public Adapter enableQueryDTOExtendsEntity() {
-            this.config.queryDTOExtendsEntity = true;
-            return this;
-        }
-
-        /**
-         * 查询vo继承实体类
-         *
-         * @return this
-         */
-        public Adapter enableQueryVOExtendsEntity() {
-            this.config.queryVOExtendsEntity = true;
-            return this;
-        }
-
-        /**
-         * 添加编辑排除字段
-         *
-         * @param fields 字段
-         */
-        public Adapter addEditExcludeFields(String... fields) {
-            this.config.editExcludeFields.addAll(Arrays.asList(fields));
-            return this;
-        }
-
-        /**
-         * 添加编辑排除列
-         *
-         * @param columns 列
-         */
-        public Adapter addEditExcludeColumns(String... columns) {
-            this.config.editExcludeColumns.addAll(Arrays.asList(columns));
-            return this;
-        }
-        
-    }
+    
 
 }
--- src/main/java/io/github/luminion/generator/config/support/adapter/ModelAdapter.java
+++ src/main/java/io/github/luminion/generator/config/support/adapter/ModelAdapter.java
@@ -1,8 +1,57 @@
 package io.github.luminion.generator.config.support.adapter;
 
+import io.github.luminion.generator.config.support.ModelConfig;
+
+import java.util.Arrays;
+
 /**
  * @author luminion
  * @since 1.0.0
  */
 public class ModelAdapter {
+    private final ModelConfig modelConfig;
+
+    public ModelAdapter(ModelConfig config) {
+        this.modelConfig = config;
+    }
+
+    /**
+     * 查询dto继承实体类
+     *
+     * @return this
+     */
+    public ModelAdapter queryDTOExtendsEntity() {
+        this.modelConfig.setQueryDTOExtendsEntity(true);
+        return this;
+    }
+
+    /**
+     * 查询vo继承实体类
+     *
+     * @return this
+     */
+    public ModelAdapter queryVOExtendsEntity() {
+        this.modelConfig.setQueryVOExtendsEntity(true);
+        return this;
+    }
+
+    /**
+     * 添加编辑排除字段
+     *
+     * @param fields 字段
+     */
+    public ModelAdapter addEditExcludeFields(String... fields) {
+        this.modelConfig.getEditExcludeFields().addAll(Arrays.asList(fields));
+        return this;
+    }
+
+    /**
+     * 添加编辑排除列
+     *
+     * @param columns 列
+     */
+    public ModelAdapter addEditExcludeColumns(String... columns) {
+        this.modelConfig.getEditExcludeColumns().addAll(Arrays.asList(columns));
+        return this;
+    }
 }
