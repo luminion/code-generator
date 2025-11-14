942549f76bffb717e9d67975b58cfce6619ca282
lo <604647004@qq.com>
2025-11-14 10:57:46 +0800

    feat(builder): 添加全局配置构建器方法- 新增lombok相关配置方法 - 添加文档类型和作者配置 - 支持java EE和excel框架配置 - 添加输出目录和文件覆盖配置 - 支持包名和模块名配置 - 添加参数校验和方法生成控制配置

--- src/main/java/io/github/luminion/generator/config/builder/GlobalBuilder.java
+++ src/main/java/io/github/luminion/generator/config/builder/GlobalBuilder.java
@@ -1,6 +1,10 @@
 package io.github.luminion.generator.config.builder;
 
 import io.github.luminion.generator.config.Configurer;
+import io.github.luminion.generator.enums.DocType;
+import io.github.luminion.generator.enums.ExcelApi;
+import io.github.luminion.generator.enums.JavaEEApi;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import java.text.SimpleDateFormat;
@@ -15,9 +19,50 @@ public class GlobalBuilder {
     private final Configurer configurer;
 
     /**
-     * 作者
+     * lombok模型
+     *
+     * @param enable 是否启用
+     * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder author(String author) {
+    public GlobalBuilder lombok(boolean enable) {
+        this.configurer.getGlobalConfig().setLombok(enable);
+        return this;
+    }
+
+    /**
+     * lombok链式getter和setter(需启用lombok)
+     *
+     * @param enable 是否启用
+     * @return this
+     * @since 1.0.0
+     */
+    public GlobalBuilder lombokChainModel(boolean enable) {
+        this.configurer.getGlobalConfig().setLombokChainModel(enable);
+        return this;
+    }
+
+
+    /**
+     * 配置文档类型
+     *
+     * @param docType 文档类型
+     * @return this
+     * @since 1.0.0
+     */
+    public GlobalBuilder docType(@NonNull DocType docType) {
+        this.configurer.getGlobalConfig().setDocType(docType);
+        return this;
+    }
+
+    /**
+     * 配置文档作者
+     *
+     * @param author 文档作者
+     * @return this
+     * @since 1.0.0
+     */
+    public GlobalBuilder docAuthor(@NonNull String author) {
         this.configurer.getGlobalConfig().setDocAuthor(author);
         return this;
     }
@@ -25,189 +70,188 @@ public class GlobalBuilder {
     /**
      * 指定注释日期格式化
      *
-     * @param pattern 格式
+     * @param pattern 格式,默认"yyyy-MM-dd"
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder commentDatePattern(String pattern) {
+    public GlobalBuilder docDate(@NonNull String pattern) {
         String format = new SimpleDateFormat(pattern).format(new Date());
         this.configurer.getGlobalConfig().setDocDate(format);
         return this;
     }
 
-    /**
-     * 开启lombok模型
-     *
-     * @return this
-     * @since 3.5.0
-     */
-    public GlobalBuilder enableLombok() {
-        this.configurer.getGlobalConfig().setLombok(true);
-        return this;
-    }
-
-    /**
-     * 禁用lombok模型
-     *
-     * @return this
-     * @deprecated 默认值,无需设置
-     */
-    @Deprecated
-    public GlobalBuilder disableLombok() {
-        this.configurer.getGlobalConfig().setLombok(false);
-        return this;
-    }
-
-    /**
-     * 开启链式getter和setter
-     *
-     * @return this
-     * @since 3.5.0
-     */
-    public GlobalBuilder enableChainModel() {
-        this.configurer.getGlobalConfig().setLombokChainModel(true);
-        return this;
-    }
 
     /**
      * 文档注释添加相关类链接
      *
+     * @param enable 是否启用
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder enableCommentLink() {
-        this.configurer.getGlobalConfig().setDocLink(true);
+    public GlobalBuilder docLink(boolean enable) {
+        this.configurer.getGlobalConfig().setDocLink(enable);
         return this;
     }
 
     /**
-     * 禁用文档注释添加相关类链接
+     * java EE 框架
      *
+     * @param javaEEApi javaEE 框架
      * @return this
-     * @deprecated 默认值,无需设置
+     * @since 1.0.0
      */
-    @Deprecated
-    public GlobalBuilder disableCommentLink() {
-        this.configurer.getGlobalConfig().setDocLink(false);
+    public GlobalBuilder javaEEApi(@NonNull JavaEEApi javaEEApi) {
+        this.configurer.getGlobalConfig().setJavaEEApi(javaEEApi);
         return this;
     }
 
-//    /**
-//     * 启用类注释随机UUID
-//     *
-//     * @return this
-//     */
-//    public GlobalBuilder enableCommentUUID() {
-//        this.configurer.getGlobalConfig().setCommentUUID(true);
-//        return this;
-//    }
-
     /**
-     * 禁用新增和修改的入参校验
+     * excel框架
      *
+     * @param excelApi excel框架
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder disableValidated() {
-        this.configurer.getGlobalConfig().setParamValidate(false);
+    public GlobalBuilder excelApi(@NonNull ExcelApi excelApi) {
+        this.configurer.getGlobalConfig().setExcelApi(excelApi);
         return this;
     }
 
-    /**
-     * 使用swagger文档
-     */
-    public GlobalBuilder enableSwagger() {
-        this.configurer.getGlobalConfig().setSwagger(true);
-        return this;
-    }
 
     /**
-     * 使用springdoc文档
-     */
-    public GlobalBuilder enableSpringdoc() {
-        this.configurer.getGlobalConfig().setSpringdoc(true);
-        return this;
-    }
-
-    /**
-     * 使用javax包作为javaEE api
-     * <p>springboot2.x使用javax, springboot3.x使用jakarta</p>
-     * 默认使用jakarta
+     * 输出根目录
      *
+     * @param outputDir 输出目录,全路径
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder enableJavaxApi() {
-        this.configurer.getGlobalConfig().setJakartaApiPackagePrefix("javax");
+    public GlobalBuilder outputDir(@NonNull String outputDir) {
+        this.configurer.getGlobalConfig().setOutputDir(outputDir);
         return this;
     }
 
     /**
-     * 使用EasyExcel
-     * <p>默认使用FastExcel</p>
+     * 是否打开输出目录
      *
+     * @param enable 是否启用
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder enableEasyExcel() {
-        this.configurer.getGlobalConfig().setExcelApiPackagePrefix("com.alibaba.excel");
-        this.configurer.getGlobalConfig().setExcelApiClass("EasyExcel");
-        return this;
-    }
-
-
-    /**
-     * 不生成查询方法
-     *
-     * @return this
-     */
-    public GlobalBuilder disableQuery() {
-        this.configurer.getGlobalConfig().setGenerateQuery(false);
+    public GlobalBuilder openOutputDir(boolean enable) {
+        this.configurer.getGlobalConfig().setOpenOutputDir(enable);
         return this;
     }
 
     /**
-     * 不生成新增方法
+     * 是否覆盖已有文件(全局,为true时始终覆盖)
      *
+     * @param enable 是否启用
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder disableInsert() {
-        this.configurer.getGlobalConfig().setGenerateInsert(false);
+    public GlobalBuilder fileOverride(boolean enable) {
+        this.configurer.getGlobalConfig().setFileOverride(enable);
         return this;
     }
 
     /**
-     * 不生成更新方法
+     * 设置父包名
      *
+     * @param packageName 父包名
      * @return this
+     * @since 1.0.0
      */
-    public GlobalBuilder disableUpdate() {
-        this.configurer.getGlobalConfig().setGenerateUpdate(false);
+    public GlobalBuilder parentPackage(@NonNull String packageName) {
+        this.configurer.getGlobalConfig().setParentPackage(packageName);
         return this;
     }
 
     /**
-     * 不生成删除方法
+     * 父包模块名
      *
+     * @param parentPackageModule 父包模块名
+     * @return this
+     * @since 1.0.0
+     */
+    public GlobalBuilder parentPackageModule(@NonNull String parentPackageModule) {
+        this.configurer.getGlobalConfig().setParentPackage(parentPackageModule);
+        return this;
+    }
+    
+    /**
+     * 生成参数校验相关注解
+     *
+     * @param enable 是否启用
      * @return this
      */
-    public GlobalBuilder disableDelete() {
-        this.configurer.getGlobalConfig().setGenerateDelete(false);
+    public GlobalBuilder validated(boolean enable) {
+        this.configurer.getGlobalConfig().setValidated(enable);
         return this;
     }
 
     /**
-     * 不生成导入方法
+     * 生成查询方法
      *
+     * @param enable 是否启用
      * @return this
      */
-    public GlobalBuilder disableImport() {
-        this.configurer.getGlobalConfig().setGenerateImport(false);
+    public GlobalBuilder generateQuery(boolean enable) {
+        this.configurer.getGlobalConfig().setGenerateQuery(enable);
         return this;
     }
 
     /**
-     * 不生成导出方法
+     * 生成新增方法
      *
+     * @param enable 是否启用
      * @return this
      */
-    public GlobalBuilder disableExport() {
-        this.configurer.getGlobalConfig().setGenerateExport(false);
+    public GlobalBuilder disableInsert(boolean enable) {
+        this.configurer.getGlobalConfig().setGenerateInsert(enable);
+        return this;
+    }
+
+    /**
+     * 生成更新方法
+     *
+     * @param enable 是否启用
+     * @return this
+     */
+    public GlobalBuilder disableUpdate(boolean enable) {
+        this.configurer.getGlobalConfig().setGenerateUpdate(enable);
+        return this;
+    }
+
+    /**
+     * 生成删除方法
+     *
+     * @param enable 是否启用
+     * @return this
+     */
+    public GlobalBuilder disableDelete(boolean enable) {
+        this.configurer.getGlobalConfig().setGenerateDelete(enable);
+        return this;
+    }
+
+    /**
+     * 生成导入方法
+     *
+     * @param enable 是否启用
+     * @return this
+     */
+    public GlobalBuilder disableImport(boolean enable) {
+        this.configurer.getGlobalConfig().setGenerateImport(enable);
+        return this;
+    }
+
+    /**
+     * 生成导出方法
+     *
+     * @param enable 是否启用
+     * @return this
+     */
+    public GlobalBuilder disableExport(boolean enable) {
+        this.configurer.getGlobalConfig().setGenerateExport(enable);
         return this;
     }
 
