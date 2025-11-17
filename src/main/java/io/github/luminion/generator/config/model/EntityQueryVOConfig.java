cf3d9479fd61611149f5aa80f54e191a319ec099
lo <604647004@qq.com>
2025-11-17 23:48:47 +0800

    feat(generator): 实现EntityQueryDTO和EntityQueryVO的模板渲染功能
    - 为EntityQueryDTOConfig和EntityQueryVOConfig类添加TemplateRender接口实现
    - 新增renderData方法，用于处理模板数据渲染逻辑
    - 添加对实体类继承的支持，可通过extendsEntity属性控制
    - 实现导入包的自动收集和分类，区分Java包和框架包
    - 更新Velocity模板文件，调整导入包变量引用方式
    - 在模板中增加序列化支持，自动添加Serializable接口和serialVersionUID
    - 优化字段循环遍历逻辑，排除逻辑删除字段
    - 整合全局配置中的Lombok和文档注解包导入规则

--- src/main/java/io/github/luminion/generator/config/model/EntityQueryVOConfig.java
+++ src/main/java/io/github/luminion/generator/config/model/EntityQueryVOConfig.java
@@ -1,15 +1,27 @@
 package io.github.luminion.generator.config.model;
 
+import io.github.luminion.generator.common.TemplateRender;
+import io.github.luminion.generator.config.Configurer;
+import io.github.luminion.generator.config.Resolver;
+import io.github.luminion.generator.config.core.GlobalConfig;
 import io.github.luminion.generator.enums.TemplateFileEnum;
+import io.github.luminion.generator.po.TableField;
+import io.github.luminion.generator.po.TableInfo;
 import io.github.luminion.generator.po.TemplateFile;
 import lombok.Data;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /**
  * @author luminion
  * @since 1.0.0
  */
 @Data
-public class EntityQueryVOConfig {
+public class EntityQueryVOConfig implements TemplateRender {
 
     /**
      * 模板文件
@@ -21,5 +33,50 @@ public class EntityQueryVOConfig {
             "/templates/mybatisplus/entityQueryVO.java",
             ".java"
     );
+
+    /**
+     * 是否继承实体类
+     */
+    protected boolean extendsEntity = true;
+
+    @Override
+    public Map<String, Object> renderData(TableInfo tableInfo) {
+        Map<String, Object> data = TemplateRender.super.renderData(tableInfo);
+        Configurer configurer = tableInfo.getConfigurer();
+        GlobalConfig globalConfig = configurer.getGlobalConfig();
+        Resolver resolver = configurer.getResolver();
+        TreeSet<String> importPackages = new TreeSet<>();
     
+        if (extendsEntity) {
+            String entityClassName = resolver.getClassName(TemplateFileEnum.ENTITY_QUERY_DTO, tableInfo);
+            String entityClassSimpleName = resolver.getClassSimpleName(TemplateFileEnum.ENTITY, tableInfo);
+            importPackages.add(entityClassName);
+            if (globalConfig.isLombok()){
+                importPackages.add("lombok.EqualsAndHashCode");
+            }
+            data.put("queryVOExtendsEntity", extendsEntity);
+        } else {
+            for (TableField field : tableInfo.getFields()) {
+                if (field.isLogicDeleteField()) {
+                    continue;
+                }
+                Optional.ofNullable(field.getJavaType().getPkg()).ifPresent(importPackages::add);
+            }
+        }
+
+        // 全局包
+        importPackages.addAll(globalConfig.getModelSerializableImportPackages());
+        importPackages.addAll(globalConfig.getModelDocImportPackages());
+        importPackages.addAll(globalConfig.getModelLombokImportPackages());
+
+
+        // 导入包
+        Collection<String> javaPackages = importPackages.stream().filter(pkg -> pkg.startsWith("java")).collect(Collectors.toList());
+        Collection<String> frameworkPackages = importPackages.stream().filter(pkg -> !pkg.startsWith("java")).collect(Collectors.toList());
+        data.put("queryVOJavaPkg", javaPackages);
+        data.put("queryVOFramePkg", frameworkPackages);
+        
+        
+        return data;
+    }
 }
