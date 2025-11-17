218fca2793c19b8191a0214643fb58f0ee807787
lo <604647004@qq.com>
2025-11-17 20:12:17 +0800

    feat(generator): 实现Excel导入导出DTO配置及模板支持
    - 新增EntityExcelExportDTOConfig和EntityExcelImportDTOConfig类
    - 实现TemplateRender接口以支持模板数据渲染
    - 添加Excel相关注解和包导入处理逻辑
    - 更新velocity模板文件以支持新的DTO结构
    - 优化lombok和序列化配置处理
    - 修复模板中getter/setter生成逻辑
    - 移除旧的toString方法生成逻辑
    - 添加日志记录和条件判断优化

--- src/main/java/io/github/luminion/generator/config/model/EntityExcelExportDTOConfig.java
+++ src/main/java/io/github/luminion/generator/config/model/EntityExcelExportDTOConfig.java
@@ -1,12 +1,15 @@
 package io.github.luminion.generator.config.model;
 
+import io.github.luminion.generator.common.JavaFieldInfo;
 import io.github.luminion.generator.common.TemplateRender;
 import io.github.luminion.generator.config.core.GlobalConfig;
 import io.github.luminion.generator.enums.TemplateFileEnum;
+import io.github.luminion.generator.po.TableField;
 import io.github.luminion.generator.po.TableInfo;
 import io.github.luminion.generator.po.TemplateFile;
 import io.github.luminion.generator.util.ClassUtils;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collection;
 import java.util.Map;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
  * @since 1.0.0
  */
 @Data
+@Slf4j
 public class EntityExcelExportDTOConfig implements TemplateRender {
     /**
      * 模板文件
@@ -51,33 +55,35 @@ public class EntityExcelExportDTOConfig implements TemplateRender {
     public Map<String, Object> renderData(TableInfo tableInfo) {
         Map<String, Object> data = TemplateRender.super.renderData(tableInfo);
         GlobalConfig globalConfig = tableInfo.getConfigurer().getGlobalConfig();
-        
         Set<String> importPackages = new TreeSet<>();
-        if (superClass!=null){
-            data.put("entityExcelExportDTOSuperClassSimpleName", ClassUtils.getSimpleName(this.superClass));
+
+        if (superClass != null) {
+            data.put("excelExportDTOSuperClass", ClassUtils.getSimpleName(this.superClass));
             importPackages.add(this.superClass);
         }
         if (this.serialUID) {
+            data.put("excelExportDTOSerial", true);
             importPackages.add("java.io.Serializable");
             if (this.serialAnnotation) {
+                data.put("excelExportDTOSerialAnnotation", true);
                 importPackages.add("java.io.Serial");
             }
         }
 
         if (globalConfig.isLombok()) {
             if (globalConfig.isChainModel()) {
-                importPackages.add("lombok.experimental.Accessors");
+                log.info("excel导出, 自动忽略chainModel, (在Excel框架中, 链式setter存在无法赋值的情况)");
             }
             if (this.superClass != null) {
                 importPackages.add("lombok.EqualsAndHashCode");
             }
-            if (superClass!=null){
+            if (superClass != null) {
                 importPackages.add("lombok.EqualsAndHashCode");
             }
             importPackages.add("lombok.Data");
         }
 
-        switch (globalConfig.getDocType()){
+        switch (globalConfig.getDocType()) {
             case SPRING_DOC:
                 importPackages.add("io.swagger.v3.oas.annotations.media.Schema");
                 break;
@@ -88,13 +94,23 @@ public class EntityExcelExportDTOConfig implements TemplateRender {
         }
         String excelIgnoreUnannotated = globalConfig.getExcelApi().packagePrefix + "annotation.ExcelIgnoreUnannotated";
         String excelProperty = globalConfig.getExcelApi().packagePrefix + "annotation.ExcelProperty";
-
+        importPackages.add(excelIgnoreUnannotated);
+        importPackages.add(excelProperty);
+        for (TableField field : tableInfo.getFields()) {
+            if (field.isLogicDeleteField()) {
+                continue;
+            }
+            JavaFieldInfo columnType = field.getJavaType();
+            if (null != columnType && null != columnType.getPkg()) {
+                importPackages.add(columnType.getPkg());
+            }
+        }
 
         // 导入包
         Collection<String> javaPackages = importPackages.stream().filter(pkg -> pkg.startsWith("java")).collect(Collectors.toList());
         Collection<String> frameworkPackages = importPackages.stream().filter(pkg -> !pkg.startsWith("java")).collect(Collectors.toList());
-        data.put("entityExcelExportDTOImportPackages4Java", javaPackages);
-        data.put("entityExcelExportDTOImportPackages4Framework", frameworkPackages);
+        data.put("excelExportDTOJavaPkg", javaPackages);
+        data.put("excelExportDTOFramePkg", frameworkPackages);
         return data;
     }
 }
