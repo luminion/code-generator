d6baa0e69ce0119c31a33dff61d1a9da722d7155
lo <604647004@qq.com>
2025-11-25 17:30:55 +0800

    fix(sqlbooster): 修正 SqlHelper 类的包路径
    - 将 SQL_BOOSTER_SQL_HELPER 的类路径从 sql.helper 更正为 helper 包下
    - 统一了 SqlHelper 类的引用路径，避免因包路径不一致导致的类加载问题

--- src/main/java/io/github/luminion/generator/enums/RuntimeClass.java
+++ src/main/java/io/github/luminion/generator/enums/RuntimeClass.java
@@ -58,7 +58,7 @@ public enum RuntimeClass {
     SPRING_BOOT_BEAN_UTILS("org.springframework.beans.BeanUtils"),
 
     SQL_BOOSTER_SQL_WRAPPER("io.github.luminion.sqlbooster.model.api.Wrapper"),
-    SQL_BOOSTER_SQL_HELPER("io.github.luminion.sqlbooster.model.sql.helper.SqlHelper"),
+    SQL_BOOSTER_SQL_HELPER("io.github.luminion.sqlbooster.model.helper.SqlHelper"),
     SQL_BOOSTER_BOOSTER_PAGE("io.github.luminion.sqlbooster.core.BoosterPage"),
     SQL_BOOSTER_BOOSTER_MP_MAPPER("io.github.luminion.sqlbooster.extension.mybatisplus.BoosterMpMapper"),
     SQL_BOOSTER_BOOSTER_MP_SERVICE_IMPL("io.github.luminion.sqlbooster.extension.mybatisplus.BoosterMpServiceImpl"),
